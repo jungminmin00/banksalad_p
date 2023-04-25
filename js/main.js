e8a0cf7a3118ec8ee3e9ae1578e2b51ad1c950d9
jungminmin00 <jjungjj0311@gmail.com>
2023-04-25 13:14:12 +0900

    project01 완료

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import $ from "jquery";
 
 $(function(){
 
-    // header nav
+// header nav
     $('header nav').on('mouseenter', function(){
         $('header ul>li>ul').css('display', 'inline-block');
         $('#headerWrap').css('height', '380px');
@@ -17,21 +17,40 @@ $(function(){
     });
 
 
-// box01 - gallery : 자동
-let fullWidth = $('#box01 .all>li').width();
-$('#box01 .all').css('margin-left','-='+fullWidth+'px');
-$('#box01 .all>li:last').prependTo('#box01 .all');
-setInterval(function(){
-    $('#box01 .all').animate({marginLeft:'-='+fullWidth+'px'}, function(){
-        $('#box01 .all>li:first').appendTo('#box01 .all');
-        $('#box01 .all').css('margin-left', '-'+fullWidth+'px');
-    });
-    for(let i = 1; i<=3; i++){
-        $('#box .subMenu>ul>li:nth-child('+i+')').css('width', '60px');
-    }
-}, 3000);
+// box01 - gallery : 자동 슬라이드
+    let fullWidth = $('#box01 .all>li').width();
+    let str;
 
-    // box04 - gallery
+    $('#box01 .all>li:last').prependTo('#box01 .all');
+    $('#box01 .all').css('margin-left', '-'+fullWidth+'px');
+    // 함수명
+    timer();
+
+    $('#box01 .all>li').each(function(){
+        $(this).on('mouseenter', function(){
+        stop();
+        });
+    });
+    $('#box01 .all>li').each(function(){
+        $(this).on('mouseleave', function(){
+        timer();
+        });
+    });
+    // 타이머 함수
+    function timer(){
+        str = setInterval(ani, 1500);
+    }
+    function stop(){
+        clearInterval(str);
+    }
+    // subMenu
+    $('.subMenu li').on('mouseenter', function(){
+        $(this).css('width', '60px');
+    }).on('mouseleave', function(){
+        $(this).css('width', '30px');
+    });
+
+// box04 - gallery
     // 버튼 클릭시 애니로 돌아가게 만들기
     let liWidth = $('#box04 .all>li').width();
     $('#box04 .all').css('margin-left','-='+liWidth+'px');  // 준비하고 시작해야 함
@@ -49,7 +68,16 @@ setInterval(function(){
         });
     });
 
-
-
+    // 다른 a막기
+    
 // 
 });
+
+// 함수
+function ani(){
+    let fullWidth = $('#box01 .all>li').width();
+    $('#box01 .all').stop().animate({marginLeft: '-='+fullWidth+'px'}, function(e){
+      $('#box01 .all>li:first').appendTo('#box01 .all');
+      $('#box01 .all').css('margin-left', '-'+fullWidth+'px')
+    });
+}
